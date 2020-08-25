4a377987d8aa92a9512ef98faccf654d96788d56
Ademola Sobaki <demolasobaki@gmail.com>
2020-08-25 21:57:27 +0100

    connect response from api to state

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,15 @@ import SearchBar from './SearchBar'
 import youtube from '../apis/youtube'
 
 class App extends Component {
+	state = { videos: [] }
+
 	onTermSubmit = async (term) => {
-		const videos = await youtube.get('/search', {
+		const response = await youtube.get('/search', {
 			params: {
 				q: term,
 			},
 		})
-		console.log(videos)
+		this.setState({ videos: response.data.items })
 	}
 
 	render() {
