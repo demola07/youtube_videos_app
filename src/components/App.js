7db65a0017cef3988e1cb5eb7757160e654c4061
Ademola Sobaki <demolasobaki@gmail.com>
2020-08-25 22:53:42 +0100

    communicate selected video from child component videlItem to parent App

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import VideoList from './VideoList'
 import youtube from '../apis/youtube'
 
 class App extends Component {
-	state = { videos: [] }
+	state = { videos: [], selectedVideo: null }
 
 	onTermSubmit = async (term) => {
 		const response = await youtube.get('/search', {
@@ -16,11 +16,18 @@ class App extends Component {
 		this.setState({ videos: response.data.items })
 	}
 
+	onVideoSelect = (video) => {
+		console.log('from app', video)
+	}
+
 	render() {
 		return (
 			<div className="ui container">
 				<SearchBar onTermSubmit={this.onTermSubmit} />
-				<VideoList videos={this.state.videos} />
+				<VideoList
+					videos={this.state.videos}
+					onVideoSelect={this.onVideoSelect}
+				/>
 			</div>
 		)
 	}
